1a50bd538559462bec8baf390f182ade48c38c7f
1786aslam <121086263+1786aslam@users.noreply.github.com>
2022-12-20 13:20:15 +0530

    Add files via upload

---
+++ FirstAutomation.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+from time import sleep
+
+driver = webdriver.Chrome(executable_path ="chromedriver.exe")
+driver.maximize_window()
+driver.get("https://facebook.com/")
+sleep(3)
+driver.find_element(By.ID, "email").send_keys("aslam@gmail.com")
+sleep(4)
+driver.find_element(By.NAME, "pass").send_keys("123456")
+sleep(5)
+driver.find_element(By.NAME, "login").click()
+sleep
+driver.close()
+
+
+
+
+
+# C:\browser driver 2\chromedriver_win32 (3)
